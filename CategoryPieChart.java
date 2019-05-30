e8fe11f87416f094e0dff77c291fd9b4bf1bef7c
mattsknobloch <49885138+mattsknobloch@users.noreply.github.com>
2019-05-30 18:10:13 -0400

    Add the files for initial graph implementation
    add files for both a pie chart based on category of transaction and a line graph that shows the daily balance for a month.  also added two needed jar files for the graphics

---
+++ CategoryPieChart.java
@@ -0,0 +1,93 @@
+import java.text.DecimalFormat;
+import javax.swing.*;
+import org.jfree.chart.*;
+import org.jfree.chart.labels.*;
+import org.jfree.chart.plot.*;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class CategoryPieChart extends JFrame
+{
+	DefaultPieDataset dataset;
+	JFrame pieChartFrame;
+	
+	//initialize the pie chart
+	public void initializePieChart()
+	{
+		//initialize the JFrame and the dataset
+		pieChartFrame = new JFrame("Pie Chart");
+		dataset = new DefaultPieDataset();
+	}
+	
+	//show the actual pie chart
+	public void drawPieChart()
+	{		
+		//create the Pie chart
+		JFreeChart chart = ChartFactory.createPieChart("Percent Spent in Each Category", dataset, true, true, false);
+		PiePlot piePlot = (PiePlot) chart.getPlot();
+		
+		//Set the label properties
+		piePlot.setSimpleLabels(true);
+        PieSectionLabelGenerator gen = new
+        		 StandardPieSectionLabelGenerator("{0}: {1} ({2})", new DecimalFormat("0"), new DecimalFormat("0%"));
+            piePlot.setLabelGenerator(gen);
+		
+        //add the chart to the panel and frame
+		ChartPanel pieChartPanel = new ChartPanel(chart);
+		pieChartFrame.add(pieChartPanel);		
+		pieChartFrame.pack();
+		pieChartFrame.setTitle("Category Percents");
+		pieChartFrame.setSize(750, 500);
+		pieChartFrame.setVisible(true);
+	}
+	
+	//function to set the value for a specific key
+
+	public void addChangeDataValue(Comparable key, double value)
+	{
+		dataset.setValue(key, value);
+	}
+	
+	//remove a key from the dataset
+	
+	public void removeDataValue(Comparable key)
+	{
+		dataset.remove(key);
+	}
+
+	//add all of the transactions from a specific month
+	public void addMonth(MonthClass month)
+	{
+		TransactionList transactionList = month.monthTransactions;
+		int numTransactions = transactionList.getLength();
+		int currentTransaction = 0;
+		while(numTransactions>currentTransaction)
+		{
+			TransactionObject transaction = transactionList.getTarget(currentTransaction);
+			String description = transaction.getTranType();
+			float tranValue = transaction.getDollarValue();
+			float oldValue = (float) dataset.getValue(description);
+			float newValue = oldValue+tranValue;
+			dataset.setValue(description, newValue);
+			currentTransaction++;
+		}
+		
+	}
+	
+	//remove all of the transactions from a specific month
+	public void removeMonth(MonthClass month)
+	{
+		TransactionList transactionList = month.monthTransactions;
+		int numTransactions = transactionList.getLength();
+		int currentTransaction = 0;
+		while(numTransactions>currentTransaction)
+		{
+			TransactionObject transaction = transactionList.getTarget(currentTransaction);
+			String description = transaction.getTranType();
+			float tranValue = transaction.getDollarValue();
+			float oldValue = (float) dataset.getValue(description);
+			float newValue = oldValue-tranValue;
+			dataset.setValue(description, newValue);
+			currentTransaction++;
+		}
+	}
+}
---
+++ MonthBalanceLineGraph.java
@@ -0,0 +1,105 @@
+import java.util.Arrays;
+import javax.swing.*;
+import org.jfree.chart.*;
+import org.jfree.chart.plot.*;
+import org.jfree.data.xy.*;
+
+public class MonthBalanceLineGraph extends JFrame
+{
+	
+	XYSeriesCollection dataset;
+	JFrame lineGraphFrame;
+	
+	String[] longMonths = {"January", "March", "May", "July", "August", "October", "December"};
+	String[] mediumMonths = {"April", "June", "September", "November"};
+	String[] shortMonths = {"February"};
+
+	//initialize the pie chart
+	public void initializeLineGraph()
+	{
+		//initialize the JFrame and the dataset
+		lineGraphFrame = new JFrame("Line Graph");
+		dataset = new XYSeriesCollection();
+	}
+	
+	//show the actual line graph	
+	public void drawLineGraph()
+	{
+		//initializes and sizes the GUI frame
+		lineGraphFrame = new JFrame("Line Graph");
+		JFreeChart chart = ChartFactory.createXYLineChart("Month Balance", "Day", "Balance", dataset, PlotOrientation.VERTICAL, true, true, false);
+		ChartPanel chartPanel = new ChartPanel(chart);
+		lineGraphFrame.add(chartPanel);
+		
+		lineGraphFrame.pack();
+		lineGraphFrame.setTitle("Line Graph");
+		
+		lineGraphFrame.setSize(750, 500);
+		lineGraphFrame.setVisible(true);
+	}
+	
+	//add all of the transactions from a specific month
+	public void addMonth(MonthClass month)
+	{
+		TransactionList transactionList = month.monthTransactions;
+		int numTransactions = transactionList.getLength();
+		int currentTransaction = 0;
+		XYSeries newMonth = new XYSeries(month.getMonth()+month.getYear());
+		if(Arrays.asList(longMonths).contains(month.getMonth()))
+		{
+			int day = 1;
+			while(day<=31)
+			{
+				newMonth.add(day, 0);
+			}
+		}
+		else if(Arrays.asList(mediumMonths).contains(month.getMonth()))
+		{
+			int day = 1;
+			while(day<=30)
+			{
+				newMonth.add(day, 0);
+			}
+		}
+		else if(Arrays.asList(shortMonths).contains(month.getMonth()))
+		{
+			int day = 1;
+			while(day<=28)
+			{
+				newMonth.add(day, 0);
+			}
+		}
+		while(numTransactions>currentTransaction)
+		{
+			TransactionObject transaction = transactionList.getTarget(currentTransaction);
+			int transactionDate = transaction.getDay();
+			float transactionValue = transaction.getDollarValue();
+			int index = newMonth.indexOf(transactionDate);
+			float previousValue = (float) newMonth.getY(index);
+			newMonth.update((Number)transactionDate, previousValue+transactionValue);
+			updateLaterDays(newMonth, transactionDate);
+			currentTransaction++;
+		}
+		dataset.addSeries(newMonth);
+		
+	}
+	
+	//update the future days so that we can keep an accurate record of how much was earned or spent in the month
+	private void updateLaterDays(XYSeries series, int transactionDate) 
+	{
+		if(transactionDate<series.getItemCount())
+		{
+			float transactionValue = (float) series.getY(series.indexOf(transactionDate));
+			float nextDayValue = (float) series.getY(series.indexOf(transactionDate+1));
+			series.update((Number)(transactionDate+1), transactionValue+nextDayValue);
+			updateLaterDays(series, transactionDate+1);
+		}
+	}
+
+	//remove all of the transactions from a specific month
+	public void removeMonth(MonthClass month)
+	{
+		XYSeries oldMonthSeries = dataset.getSeries(month.getMonth()+month.getYear());
+		dataset.removeSeries(oldMonthSeries);
+	}
+}
